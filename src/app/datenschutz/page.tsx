ce3f1050350b1148e898901ec6cf7cbfc16a60d5
Jakob Fink-Lamotte <jakob.fink@web.de>
2025-06-10 20:46:47 +0200

    Anpassung Datenschutz

--- src/app/datenschutz/page.tsx
+++ src/app/datenschutz/page.tsx
@@ -4,7 +4,7 @@ export default function Datenschutz() {
       <h1 className="text-3xl font-bold mb-6">Datenschutzerklärung</h1>
       <p className="mb-2">Diese Website erhebt keine personenbezogenen Daten ohne Ihre ausdrückliche Zustimmung. Bei Nutzung von Terminbuchungstools (z. B. Calendly) gelten deren Datenschutzbestimmungen.</p>
       <p className="mb-2">Server-Logs und statistische Analysen erfolgen ausschließlich anonymisiert.</p>
-      <p className="mt-6 text-sm text-gray-500">Für weitere Informationen kontaktieren Sie mich bitte unter: kontakt@praxis-fink.de</p>
+      <p className="mt-6 text-sm text-gray-500">Für weitere Informationen kontaktieren Sie mich bitte unter: mail@praxis-finklamotte.de</p>
     </main>
   );
 }
